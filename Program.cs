8b12834332632d899d24cb34d51aab22e5061b2c
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-20 09:30:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EX11While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //OPG 11.1
+            int i = 1;
+            int l = 10;
+            int j = 3;
+            int k = 1;
+            Console.WriteLine("1 til 10");
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+
+                i++;
+            }
+            Console.WriteLine("\n10 til 1");
+            while (l >= 1)
+            {
+                Console.WriteLine(l);
+                l--;
+            }
+            Console.WriteLine("\nAlle lige tal fra 1 til 20");
+            while (j <= 20)
+            {
+                Console.WriteLine(j);
+                j = j + 2;
+            }
+            Console.WriteLine("\nAlle ulige tal fra 1 til 20");
+            while (k <= 20)
+            {
+                Console.WriteLine(k);
+                k = k + 2;
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
